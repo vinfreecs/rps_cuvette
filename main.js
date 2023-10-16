10d73c31c59141a6958300e1ffdb5830f0ea4767
Vinay Patil <vinfreecs>
2023-10-16 22:40:26 +0530

    all the funtionalities are finished css left

---
+++ main.js
@@ -0,0 +1,188 @@
+const options = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+let playerChoice;
+let computerChoice;
+function computerInput() {
+  return options[Math.floor(Math.random() * 3)];
+}
+function playRound(playerInput, computerInput) {
+  switch (playerInput) {
+    case "rock":
+      switch (computerInput) {
+        case "rock":
+          return "tie";
+        case "paper":
+          return "lose";
+        case "scissors":
+          return "win";
+      }
+      break;
+    case "paper":
+      switch (computerInput) {
+        case "rock":
+          return "win";
+          break;
+        case "paper":
+          return "tie";
+          break;
+        case "scissors":
+          return "lose";
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerInput) {
+        case "rock":
+          return "lose";
+          break;
+        case "paper":
+          return "win";
+          break;
+        case "scissors":
+          return "tie";
+          break;
+      }
+      break;
+    default:
+      console.log("not working");
+  }
+}
+function PlayRound() {}
+function ManageRules() {
+  const gameRules = document.querySelector(".game_rules");
+  document.querySelector(".open_rules").addEventListener("click", () => {
+    gameRules.style.display = "flex";
+  });
+  document.querySelector(".close_rules").addEventListener("click", () => {
+    gameRules.style.display = "none";
+  });
+}
+function UpdateScore() {
+  const computerScoreDom = document.querySelector(
+    ".computer_score .score_number"
+  );
+  const playerScoreDom = document.querySelector(".your_score .score_number");
+  computerScoreDom.textContent = computerScore;
+  playerScoreDom.textContent = playerScore;
+}
+function PlayAgain() {
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.value === "play_again") {
+      console.log("clicked");
+      document.querySelector(".options").style.display = "flex";
+      document.querySelector(".result").style.display = "none";
+      document.querySelector(".open_hurray").style.display = "none";
+      document.querySelector(".header").style.display = "flex";
+      document.querySelector(".hurray").style.display = "none";
+    }
+  });
+}
+function ManageNext(){
+    document.querySelector(".open_hurray").addEventListener("click",()=>{
+        document.querySelector(".options").style.display = "none";
+        document.querySelector(".header").style.display = "none";
+        document.querySelector(".hurray").style.display = "flex";
+        document.querySelector(".result").style.display = "none";
+        document.querySelector(".open_hurray").style.display = "none";
+    })
+}
+function UpdateResult(res) {
+  const result = document.querySelector(".result");
+  result.style.display = "flex";
+  switch (res) {
+    case "win":
+      document.querySelector(".open_hurray").style.display = "block";
+      result.innerHTML = `
+        <div class="player_choice">
+            <p>You've Picked</p>
+            <button  id="${playerChoice}_selected" class="">
+                <img src="${playerChoice}.png" alt="${playerChoice}">
+            </button>
+        </div>
+        <div class="result_details">
+            <p>YOU ${res} <br> AGAINST PC</p>
+            <button class="play_again">Play Again</button>
+        </div>
+        <div class="computer_choice">
+            <p>Computer Picked</p>
+            <button  id="${computerChoice}_selected" class="">
+                <img src="${computerChoice}.png" alt="${computerChoice}">
+            </button>
+        </div>
+        `;
+        break;
+    case "lose":
+        result.innerHTML = `
+        <div class="player_choice">
+            <p>You've Picked</p>
+            <button  id="${playerChoice}_selected" class="">
+                <img src="${playerChoice}.png" alt="${playerChoice}">
+            </button>
+        </div>
+        <div class="result_details">
+            <p>YOU ${res} <br> AGAINST PC</p>
+            <button class="play_again">Play Again</button>
+        </div>
+        <div class="computer_choice">
+            <p>Computer Picked</p>
+            <button  id="${computerChoice}_selected" class="">
+                <img src="${computerChoice}.png" alt="${computerChoice}">
+            </button>
+        </div>
+        `
+        break;
+    case "tie":
+        result.innerHTML = `
+        <div class="player_choice">
+            <p>You've Picked</p>
+            <button  id="${playerChoice}_selected" class="">
+                <img src="${playerChoice}.png" alt="${playerChoice}">
+            </button>
+        </div>
+        <div class="result_details">
+            <p>TIE UP</p>
+            <button class="play_again">REPLAY</button>
+        </div>
+        <div class="computer_choice">
+            <p>Computer Picked</p>
+            <button  id="${computerChoice}_selected" class="">
+                <img src="${computerChoice}.png" alt="${computerChoice}">
+            </button>
+        </div>
+    `
+
+  }
+}
+function ScreenController() {
+  const playerInput = document.querySelectorAll(".option");
+  console.log(playerInput);
+  playerInput.forEach((item) => {
+    item.addEventListener("click", () => {
+      document.querySelector(".options").style.display = "none";
+      playerChoice = item.id;
+      computerChoice = computerInput();
+      let res = playRound(playerChoice, computerChoice);
+      switch (res) {
+        case "win":
+          playerScore++;
+          break;
+        case "lose":
+          computerScore++;
+          break;
+        case "tie":
+          break;
+        default:
+          console.log("broke in screenController!");
+      }
+      UpdateScore();
+      UpdateResult(res);
+    });
+  });
+  PlayAgain();
+  ManageRules();
+  ManageNext();
+}
+UpdateScore();
+ScreenController();
+console.log(computerScore, playerScore);
